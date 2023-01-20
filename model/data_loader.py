bac1ee418635bd78b4912321a12b0ec34bb3551b
Shahar Cohen <52333216+shaharec@users.noreply.github.com>
2023-01-20 17:11:49 +0200

    update model

--- model/data_loader.py
+++ model/data_loader.py
@@ -1,7 +1,8 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 import os 
-from model_cfg import CFG
+from model.model_cfg import CFG
+import gc
 from sklearn.model_selection import train_test_split
 
 class PEFDataset(Dataset):
@@ -23,7 +24,7 @@ class PEFDataset(Dataset):
            * Large languege model embeddings
            * hand selected features
     '''
-    def __init__(self,file_df ,datapath=CFG.data_path,homothresh=CFG.homothresh,type='train'):
+    def __init__(self,file_df ,datapath=CFG.data_path,homothresh=CFG.homothresh,type='train',train_type = 'AlphaFold'):
         """_summary_
         Data set for the Deep energy function dataset.
         Args:
@@ -36,6 +37,11 @@ class PEFDataset(Dataset):
         self.filenames = file_df
         self.homothresh = homothresh
         self.type = type
+        self.train_type = train_type
+        # remove the files with homology greater than homothresh
+        if type == 'train':
+            self.check_data_constrain(self.homothresh)
+        
 
     def __len__(self):
         return len(os.listdir(self.datapath))
@@ -50,7 +56,6 @@ class PEFDataset(Dataset):
         coordsAlpha = torch.load(os.path.join(index_path, 'coordsAlpha.pt')).to(CFG.device)
         coordsBeta = torch.load(s.path.join(index_path, 'coordsBeta.pt')).to(CFG.device)
         coordsC = torch.load(os.path.join(index_path, 'coordsC.pt')).to(CFG.device)
-        coordsCa = torch.load(os.path.join(index_path, 'coordsCa.pt')).to(CFG.device)
         coordsN = torch.load(os.path.join(index_path, 'coordsN.pt')).to(CFG.device)
         # 3D coordinates of the protein native
         coordsAlpha_native = torch.load(os.path.join(index_path, 'coordsAlpha_native.pt')).to(CFG.device)
@@ -63,10 +68,76 @@ class PEFDataset(Dataset):
         nativemask = torch.load(os.path.join(index_path, 'nativemask.pt')).to(CFG.device)
         # Embeddings
         esm_embed = torch.load(os.path.join(index_path, 'emb_esm.pt')).to(CFG.device)
+        # Concatenate the coordinates
+        Xd = self.concat_cords(coordsAlpha,coordsBeta, coordsC, coordsN)
+        Xn = self.concat_cords(coordsAlpha_native,coordsBeta_native, coordsC_native, coordsCa_native, coordsN_native)
+            
+        return seq, id, Xd,Xn, mask, nativemask, esm_embed 
         
-        return seq, id, coordsAlpha, coordsBeta, coordsC, coordsCa, coordsN, coordsAlpha_native, coordsBeta_native, coordsC_native, coordsCa_native, coordsN_native, mask, nativemask, esm_embed 
+    def concat_cords(self,coordsAlpha,coordsBeta, coordsC, coordsN):
+        """
+        Concatenate the coordinates
+        output: X (torch.tensor): concatenated coordinates [N,4,3]
+        """
+        coordsAlpha,coordsBeta, coordsC, coordsCa, coordsN = coordsAlpha.unsqueeze(1), coordsBeta.unsqueeze(1), coordsC.unsqueeze(1), coordsN.unsqueeze(1)
+        X = torch.cat((coordsAlpha,coordsBeta, coordsC, coordsN), dim=1)
+        return X
+    
+    def check_data_constrain(self, homothresh):
+        """
+        Check the data constrain and remove the files with homology greater than homothresh.
+        Check mask and native mask.
+        Update file names list.
+        """
+        new_filenames = []
+        for i in range(len(self.filenames)):
+            (seq, id, coordsAlpha,coordsBeta, coordsC, coordsCa, coordsN, 
+             coordsAlpha_native, coordsBeta_native, coordsC_native, 
+             coordsCa_native, coordsN_native, mask, nativemask, esm_embed)  = self.__getitem__(i)
+            dt = torch.get_default_dtype()
+            coordN = coordN.to(dt)
+            coordAlpha = coordAlpha.to(dt)
+            coordC = coordC.to(dt)
+            coordBeta = coordBeta.to(dt)
+            seq = seq.to(dt)
+            embedding = embedding.to(dt)
+            coordNNative = coordNNative.to(dt)
+            coordAlphaNative = coordAlphaNative.to(dt)
+            coordCNative = coordCNative.to(dt)
+            coordBetaNative = coordBetaNative.to(dt)
+
+            s = seq.mean(-1)
+            if (self.homothresh is not None) and (s.max() > self.homothresh):
+                # print("protein is too homogenuous", id)
+                continue
+
+            if (self.train_type is not None) and (not self.train_type in id):
+                continue
+
+            # scale = 1e-2
+            # Mnat = nativemask
+            # M = msk & Mnat
+
+            # ind = torch.where(M)[0]
+            # istart = ind[0]
+            # ilast = ind[-1]
+            # M = M[istart:ilast + 1]
+            # msk = msk[istart:ilast + 1]
+            # msk = msk.type('torch.FloatTensor')
+            # if torch.any(msk == 0):
+            #     # print("id problem", id)
+            #     idx = (idx + 1) % self.__len__()
+            #     continue
+ 
+            new_filenames.append(self.filenames[i])
+            
+            torch.cuda.empty_cache()
+            gc.collect()
         
-def fetch_dataloader(types, data_dir, params):
+        self.filenames = new_filenames
+  
+        
+def fetch_dataloader(data_dir, params):
     """
     Fetches the DataLoader object for each type in types from data_dir.
     Args:
--- model/model_cfg.py
+++ model/model_cfg.py
@@ -2,6 +2,12 @@ import torch
 
 class CFG:
     device = torch.device("cuda:0" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")  # Use GPU is avaliable
+    if(device.type == "cuda" or device.type == "mps"):
+        torch.cuda.empty_cache()
+        cuda = True
+    else:
+        cuda = False
+        
     data_path = './data/data2'
     seed = 42
     # Train data parameters
@@ -9,3 +15,13 @@ class CFG:
     split_train = 0.8
     # Model parameters
     h = 0.001
+    coords_emb = 64
+    embedding_size = 480
+    filters = 128
+    num_layers = 16
+    
+    #training parameters
+    lr = 0.0001
+    wd = 0.0001
+    batch_size = 1
+    num_workers = 2

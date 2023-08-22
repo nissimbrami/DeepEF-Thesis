e2702358ce1ae97295295b0c1f02b7c52f407760
Shahar <shahar.cohen.03@gmail.com>
2023-08-22 20:42:54 +0300

    softplus experiment

--- train-thermodynamics.py
+++ train-thermodynamics.py
@@ -252,22 +252,23 @@ def criterion(Ejf, Ekf, Eju, Eku):
     output:
         loss (tensor): The loss of the model
     """
+    softplus = torch.nn.Softplus(beta=0.2)
     delta_g1, delta_g2, delta_g3, delta_g4 = Ejf-Eju, Ekf-Ejf, Eku-Eju, Ekf-Eku # themodynamic cycle, from the paper
     lossg = ((delta_g1+delta_g2)-(delta_g3+delta_g4))**2
-    lossd = torch.log((torch.exp(Ekf-Eku) + torch.exp(Ejf-Eju)) +1)
+    # lossd = torch.log((torch.exp(Ekf-Eku) + torch.exp(Ejf-Eju)) +1)
+    lossd = softplus(Ekf-Eku) + softplus(Ejf-Eju)
     
     return lossd+lossg , lossd, lossg  
 
 def trainAndTest(model,train_loader,valid_loader,test_loader,optimizer,device,N,epoch,scheduler):
     "train and test the model"
     valid_loss = 100
-    if epoch > 0:
-        model,optimizer,epoch,loss,valid_loss = load_checkpoint(CFG.model_path+f"best_model.pt", model, optimizer,CFG.device)
-    training(model, optimizer, train_loader,valid_loader, CFG.device,CFG.N,epoch,valid_loss,scheduler)
+    # if epoch > 0:
+    #     model,optimizer,epoch,loss,valid_loss = load_checkpoint(CFG.model_path+f"best_model.pt", model, optimizer,CFG.device)
+    # training(model, optimizer, train_loader,valid_loader, CFG.device,CFG.N,epoch,valid_loss,scheduler)
     #load the best model and check the validation
     load_checkpoint(CFG.model_path+f"best_model.pt", model, optimizer,CFG.device)
     validation(model, valid_loader,CFG.device,-1, CFG.N, optimizer , val_type = 'robust')
-    validation(model, valid_loader,CFG.device,-1, CFG.N, optimizer, val_type = 'soft')
     validation(model, train_loader,CFG.device,-1, CFG.N, optimizer, val_type = 'train')  
     # amino acid inference
     # A_inference(model, amino_inference_loader, CFG.device, CFG.N,optimizer,val_type = 'robust') 

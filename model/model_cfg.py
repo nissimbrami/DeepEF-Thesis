dc8376cb1ea9f76484b71233cd3bc487ead8da42
Shahar <shahar.cohen.03@gmail.com>
2023-11-08 17:29:11 +0200

    training 30% data

--- model/model_cfg.py
+++ model/model_cfg.py
@@ -9,7 +9,7 @@ class CFG:
     else:
         cuda = False
         
-    data_path = './data/casp12_data_100/' #'./data/data2'
+    data_path = './data/casp12_data_30/' #'./data/data2'
     inference_path = './data/inference_data'
     results_path = './res/results-emb/'
     seed = 42
@@ -26,7 +26,7 @@ class CFG:
     embedding_size = 20
     filters = 64
     num_layers = 3
-    model_path = "./res/trianed_models_crd_decoy/"
+    model_path = "./res/trianed_models_crd_decoy-30/"
     gaussian_coef = -0.008*1e1
     #training parameters
     lr = 0.0001

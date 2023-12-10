gradient penalty fix

model/model_cfg.py
@@ -2,18 +2,21 @@ import torch
 
 
 class CFG:
-    device =torch.device("cuda:0" if torch.cuda.is_available() else 'cpu') #torch.device("cuda:0" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")  # Use GPU is avaliable
+    # device =torch.device("cuda:0" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")  # Use GPU is avaliable 
+    device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu') 
     if(device.type == "cuda" or device.type == "mps"):
         torch.cuda.empty_cache()
         cuda = True
     else:
         cuda = False
-        
-    data_path = './data/casp12_data_100/' #'./data/data2'
+    debug = True
+    if debug:    
+        data_path = './data/casp12_data_30/'
+    else: 
+        data_path = './data/casp12_data_100/' #'./data/data2'
     inference_path = './data/inference_data'
     results_path = './res/results-emb/'
     seed = 42
-    debug = True
     # Train data parameters
     homothresh = 0.9
     constraint = True
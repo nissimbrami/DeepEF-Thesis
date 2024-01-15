b66c6a037c7151893270278629e88ff98c9176c9
Shahar <shahar.cohen.03@gmail.com>
2024-01-15 17:04:18 +0200

    fix first epoch

--- train-nomericalLG.py
+++ train-nomericalLG.py
@@ -452,7 +452,7 @@ def main():
     wandb_config(wandb, model, optimizer, scheduler, train_loader)
     # Run training
     print('***Start training***')
-    epoch = 3
+    epoch = 0
     trainAndTest(model,train_loader,valid_loader,test_loader,optimizer,CFG.device,CFG.N,epoch, scheduler)
     return 1
 

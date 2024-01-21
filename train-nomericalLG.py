0540dc3614d6e31ed5639357dcf317e6c9cde4b4
Shahar <shahar.cohen.03@gmail.com>
2024-01-21 17:22:55 +0200

    continue nomerical loss g

--- train-nomericalLG.py
+++ train-nomericalLG.py
@@ -22,7 +22,7 @@ torch.set_default_dtype(CFG.torch_default_dtype)
 # torch.autograd.set_detect_anomaly(True)
 # Set wandb
 if not CFG.debug:
-    wandb.init(project="Thermodynamic+decoy",name = 'epoch 0 nomerical lossg')
+    wandb.init(project="Thermodynamic+decoy",name = 'epoch 10 nomerical lossg')
 if CFG.debug:
    CFG.model_path = "./res/debug/"
    CFG.results_path = './res/results-debug/'
@@ -457,7 +457,7 @@ def main():
     wandb_config(wandb, model, optimizer, scheduler, train_loader)
     # Run training
     print('***Start training***')
-    epoch = 0
+    epoch = 10
     trainAndTest(model,train_loader,valid_loader,test_loader,optimizer,CFG.device,CFG.N,epoch, scheduler)
     return 1
 

e6e908bc3f938f9e56073c3594244b86eb7f2599
Shahar <shahar.cohen.03@gmail.com>
2024-03-25 22:42:48 +0200

    training mse gradient loss

--- train-new_loss.py
+++ train-new_loss.py
@@ -19,12 +19,12 @@ import wandb
 
 # Set the default data type to float32
 torch.set_default_dtype(CFG.torch_default_dtype)
-torch.autograd.set_detect_anomaly(True)
-CFG.debug = True
+# torch.autograd.set_detect_anomaly(True)
+# CFG.debug = True
 CFG.clip_grad_norm = True
 # Set wandb
 if not CFG.debug:
-    wandb.init(project="Thermodynamic+decoy",name = 'epoch 0 contrastive loss')
+    wandb.init(project="Thermodynamic+decoy",name = 'epoch 0 MSE grad')
 if CFG.debug:
    CFG.model_path = "./res/debug/"
    CFG.results_path = './res/results-debug/'

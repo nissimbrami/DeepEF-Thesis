3f690124a04d49347ede46b87caec37c54fd00c6
Shahar <shahar.cohen.03@gmail.com>
2024-04-30 13:10:30 +0300

    no exdu

--- train-no_EXDU.py
+++ train-no_EXDU.py
@@ -368,8 +368,8 @@ def criterion(Ejf, Eju, Exd, X_native, Ecd, Exdu, with_grad = True ):
     lossg = gradient_penalty(X_native, Ejf) if with_grad else torch.tensor(0.0).to(Ejf.device)
     lossd = lossd_fucntion(Ejf, Exd, Ecd, Exdu, Eju)
     # lossc = energy_softplus(Ejf, Ekf, Eju, Eku)
-    # Use mse loss for Eju 
-    lossc = torch.mean((Eju)**2) + torch.mean((Ejf)**2)
+    # Use softplus loss for regularization 
+    lossc = torch.log(torch.exp(-Ejf)+1) 
     
     return lossd+lossg+lossc , lossd, lossg, lossc
   
